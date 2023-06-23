401f1777def691fc4227a4cef0b0da99ffd08557
fofiefopaelisabeth <fofiefopaelisabeth@gmail.com>
2023-06-23 14:40:02 +0100

    backend

---
+++ config/appconfig.js
@@ -0,0 +1,9 @@
+require('dotenv').config;
+
+const MONGO_DB_CONFIG = {
+
+  DB: 'mongodb+srv://fofiefopaelisabeth:elisabeth@fecluster.y8x2pkl.mongodb.net/?retryWrites=true&w=majority'
+} 
+module.exports = {
+  MONGO_DB_CONFIG
+}
